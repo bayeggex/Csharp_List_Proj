d9e02a5f9e57e2f525a90c9e80d06bc5dbd051bb
Bay Eggex <79448667+bayeggex@users.noreply.github.com>
2023-11-27 19:31:34 +0300

    Update Last Code

---
+++ ConsoleApp2/CustomList.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+    public class CustomList<T>
+    {
+        public int Count => count;
+
+        private T[] values;
+        private int count;
+
+        public CustomList()
+        {
+            values = new T[4];
+            count = 0;
+        }
+
+        public void Add(T item)
+        {
+            if (count == values.Length)
+            {
+                Array.Resize(ref values, values.Length * 2);
+            }
+            values[count++] = item;
+        }
+
+        public void Remove(int index)
+        {
+            if (index >= 0 && index < count)
+            {
+                for (int i = index; i < count - 1; i++)
+                {
+                    values[i] = values[i + 1];
+                }
+                count--;
+            }
+        }
+
+        public bool Contains(T item)
+        {
+            return Array.IndexOf(values, item) != -1;
+        }
+
+        public void Clear()
+        {
+            Array.Resize(ref values, 0);
+            count = 0;
+        }
+
+        public void List()
+        {
+            foreach (var element in values)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,16 +1,19 @@
 ﻿using System;
 using System.Reflection;
 
+namespace ConsoleApp2; 
 class Program
 {
     static void Main()
     {
+        CustomList<int> intList = new CustomList<int>();
+
         for (int i = 0; i < 3; i++)
         {
             Console.Write("> ");
             if (int.TryParse(Console.ReadLine(), out int prefInput))
             {
-                
+                intList.Add(prefInput);
             }
             else
             {
@@ -18,6 +21,17 @@ class Program
             }
         }
 
-     
+        intList.Add(10);
+        intList.Remove(1);
+
+        Console.WriteLine("List:");
+        intList.List();
+
+        Console.WriteLine("Toplam List int:");
+        Console.WriteLine(intList.Count);
+
+        intList.Clear();
+        Console.WriteLine("Temiz List Int:");
+        Console.WriteLine(intList.Count);
     }
 }
