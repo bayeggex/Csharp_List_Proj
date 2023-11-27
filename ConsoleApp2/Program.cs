0d216bf28f5fb40d1e8f17859e99b7561842ac32
Bay Eggex <79448667+bayeggex@users.noreply.github.com>
2023-11-27 18:23:05 +0300

    Main Code

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Console.Write("> ");
+            if (int.TryParse(Console.ReadLine(), out int prefInput))
+            {
+                
+            }
+            else
+            {
+                i--;
+            }
+        }
+
+     
+    }
+}
